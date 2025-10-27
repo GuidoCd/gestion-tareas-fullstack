dd5f03ace07340289594c40b1b81180b42b8a4c2
Guido Cordova <gmauriciocd@gmail.com>
2025-10-27 23:02:23 +0000

    feat: Define eloquent model relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
-    //
+    public function priority(): BelongsTo
+    {
+        return $this->belongsTo(Priority::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'tag_task');
+    }
 }
