1cda94e755856bb51c5e9f715c0ef0413150e2bf
Guido Cordova <gmauriciocd@gmail.com>
2025-10-27 22:57:08 +0000

    feat: Create database schema and models for tasks, priorities, and tags

---
+++ database/migrations/2025_10_27_224928_create_tag_task_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
+            $table->foreignId('task_id')->constrained('tasks')->onDelete('cascade');
+
+            // Clave primaria compuesta para evitar duplicados
+            $table->primary(['tag_id', 'task_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tag_task');
+    }
+};
